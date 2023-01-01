924ab14a1bb884039b7a484fb99ec3ad0ad18f94
Jorgiz <109821453+Jorgiz@users.noreply.github.com>
2023-01-01 15:28:13 -0300

    .

--- Main Characters/player.gd
+++ Main Characters/player.gd
@@ -2,12 +2,12 @@ extends CharacterBody2D
 
 @export var air_jumps := 1
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -220.0
+const SPEED := 100.0
+const JUMP_VELOCITY := -220.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var air_jumps_left = air_jumps
+var air_jumps_left := air_jumps
 
 
 func _ready() -> void:
@@ -23,15 +23,20 @@ func _physics_process(delta: float) -> void:
 func _move(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if is_on_wall() and velocity.y > 0:
+			velocity.y = 30
+			
+		else:
+			velocity.y += gravity * delta
 	
 	
 	# Handle Jump.
 	var jump = Input.is_action_just_pressed("jump")
 	
-	if is_on_floor() and jump:
+	if (is_on_floor() or is_on_wall()) and jump:
 		air_jumps_left = air_jumps
 		velocity.y = JUMP_VELOCITY
+		
 	elif jump and air_jumps_left != 0:
 		air_jumps_left -= 1
 		velocity.y = JUMP_VELOCITY
@@ -44,6 +49,7 @@ func _move(delta: float) -> void:
 	if direction:
 		$AnimatedSprite2D.scale.x = direction # Flips sprite
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED*0.1)
 	
@@ -56,18 +62,26 @@ func _animate() -> void:
 	var is_running = is_on_floor() and velocity.x != 0
 	var is_jumping = velocity.y < 0 and air_jumps_left == air_jumps
 	var is_double_jumping = velocity.y < 0 and air_jumps_left != air_jumps
-	var is_falling = velocity.y > 0
-	
+	var is_falling = velocity.y > 0 and not is_on_wall()
+	var is_wall_jumping = is_on_wall()
+
 	if is_idle:
 		$AnimatedSprite2D.play("Idle")
+
 	elif is_running:
 		$AnimatedSprite2D.play("Run")
+
 	elif is_jumping:
 		$AnimatedSprite2D.play("Jump")
+
 	elif is_double_jumping:
 		$AnimatedSprite2D.play("Double Jump")
+
 	elif is_falling:
 		$AnimatedSprite2D.play("Fall")
+
+	elif is_wall_jumping:
+		$AnimatedSprite2D.play("Wall Jump")
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
